e82429c89b057df3c83c9e70766594c471c0958b
reshmamote1998 <reshmamote1998@gmail.com>
2026-09-02 23:39:18 +0530

    Add updated webinar page

--- src/Webinar.tsx
+++ src/Webinar.tsx
@@ -2,7 +2,7 @@ const Webinar = () => {
   return (
     <div style={{ width: "100%", height: "100vh" }}>
       <iframe
-        src='https://sarmak-webinar.netlify.app/'
+        src="https://sarmakpro.netlify.app/"
         title="SARMAK Webinar"
         style={{
           width: "100%",
